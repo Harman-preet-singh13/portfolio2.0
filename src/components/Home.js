89a9c90672b34d5ed6e0b1ed4395df06320fdb77
Harmanpreet Singh <harmanp397@gmail.com>
2024-01-23 18:02:16 +0530

    added carosuel in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,32 @@
-import React from "react";
-import starImg from "../images/star-pattern.png"
-import lineImg from "../images/line-pattern.png"
-import resumeFile from "../file/resume.pdf"
+import React, { useState, useEffect } from "react";
+import starImg from "../images/star-pattern.png";
+import lineImg from "../images/line-pattern.png";
+import resumeFile from "../file/resume.pdf";
+
+
+const intialStackArr = ["Front-End", "Back-End", "Full-Stack"];
 
 export default function Home() {
 
-  const handleClick = () =>{
+  const [activeItem, setActiveItem] = useState(0);
+  const [stackArr, setStackArr] = useState([intialStackArr[activeItem]]);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setActiveItem((prevItem) => (prevItem < intialStackArr.length - 1 ? prevItem + 1 : 0));
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [activeItem]);
+
+  useEffect(() => {
+    setStackArr([intialStackArr[activeItem]]);
+  }, [activeItem]);
+
+  const handleClick = () => {
     const fileUrl = resumeFile;
 
-    const link = document.createElement('a');
+    const link = document.createElement("a");
 
     link.href = fileUrl;
 
@@ -20,34 +37,42 @@ export default function Home() {
     link.click();
 
     document.body.removeChild(link);
-
-  }
+  };
 
   return (
-    <main className="max-w-[1024px] mx-auto h-[75vh]">
+    <main className="max-w-[1024px] mx-auto h-[75vh]" >
       <div className="mx-2 mt-[20vh] flex justify-center">
         <div className="home">
           <h3>Hello,</h3>
           <h3>I am Harmanpreet Singh</h3>
-          <h1 className="home-heading">Full Stack</h1>
+
+  
+            <div>
+              {stackArr.map((arr, index) => (
+                <h1
+                key={index}
+                className={`home-heading`}>
+                  {arr}
+                </h1>
+              ))}
+            </div>
+      
+
           <h1 className="home-heading">Developer</h1>
           <h3>From Punjab, India</h3>
-          <button
-          onClick={handleClick}
-          className="home-resume-btn mt-3"
-          >
+          <button onClick={handleClick} className="home-resume-btn mt-3">
             Resume
           </button>
           <div className="">
             <img
-            src={starImg}
-            alt="star pattern"
-            className="home-star-pattern"
+              src={starImg}
+              alt="star pattern"
+              className="home-star-pattern"
             />
             <img
-            src={lineImg}
-            alt="line pattern"
-            className="home-line-pattern"
+              src={lineImg}
+              alt="line pattern"
+              className="home-line-pattern"
             />
           </div>
         </div>
