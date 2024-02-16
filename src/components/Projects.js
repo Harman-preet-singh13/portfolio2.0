96d7f045ae7c54cd5e2b86daacd674ec3b6883e1
Harmanpreet Singh <harmanp397@gmail.com>
2024-02-16 17:20:20 +0530

    project-added shadow

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -62,7 +62,7 @@ export default function Projects() {
           <section className="project-container p-5 ">
             {ProjectContent.map((projects, index) => {
               return (
-                <div key={index} className=" shadow-md my-10">
+                <div key={index} className="border rounded-md shadow-inner my-10 p-2 md:p-0">
                   <div className="py-5  md:py-20 mx-0 md:mx-5">
                     <h3 className="mb-5">
                       <span>{projects.id}.{" "}</span>
@@ -71,7 +71,7 @@ export default function Projects() {
                     <div className=" flex flex-col-reverse md:flex-row justify-between">
                       <div 
                       
-                      className={`self-center max-w-[350px]`}
+                      className={`self-center mt-4 md:mt-0 max-w-[350px]`}
                       
                       >
                         <a
@@ -92,9 +92,8 @@ export default function Projects() {
                       >
                         <img
                         src={projects.projectImg}
-                        className={`max-w-[200px] md:max-w-[350px] rounded-sm shadow-xl
+                        className={`mx-auto max-w-[200px] md:max-w-[350px] rounded-sm shadow-xl
                       hover:-translate-y-4 transition-transform ease-in-out 
-                      
                       `}
                         alt={projects.name}
                         
